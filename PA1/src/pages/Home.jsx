fd003eb5008c9f6031562bd0531f9f6e9bc05568
Pema_Rinzin_Deolkar <deolkar07@gmail.com>
2026-04-03 16:51:37 +0600

    PA1

---
+++ PA1/src/pages/Home.jsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import "../scss/Home.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { BiPlay } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
+import myBanner from "../assets/myBanner.jpg";
+
+// Fetch Data from Movie-API
+const apiKey = "7e5122f42b3d47b2f9c1deaf4e1d2214";
+const url = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/original";
+const upcoming = "upcoming";
+const nowPlaying = "now_playing";
+const popular = "popular";
+const topRated = "top_rated";
+
+const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
+
+const Row = ({ title, arr = [] }) => (
+  <div className="row">
+    <h2>{title}</h2>
+
+    <div>
+      {arr.map((item, index) => (
+        <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
+      ))}
+    </div>
+  </div>
+);
+
+const Home = () => {
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [genre, setGenre] = useState([]);
+
+  useEffect(() => {
+    const fetchUpcoming = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${upcoming}?api_key=${apiKey}`);
+      setUpcomingMovies(results);
+    };
+    const fetchNowPlaying = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${nowPlaying}?api_key=${apiKey}`);
+      setNowPlayingMovies(results);
+    };
+    const fetchPopular = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${popular}?api_key=${apiKey}`);
+      setPopularMovies(results);
+    };
+    const fetchTopRated = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/${topRated}?api_key=${apiKey}`);
+      setTopRatedMovies(results);
+    };
+    const getAllGenre = async () => {
+      const {
+        data: { genres },
+      } = await axios.get(`${url}/genre/movie/list?api_key=${apiKey}`);
+      setGenre(genres);
+      // console.log(genres);
+    };
+
+    getAllGenre();
+
+    fetchUpcoming();
+    fetchNowPlaying();
+    fetchPopular();
+    fetchTopRated();
+  }, []);
+
+  return (
+    <section className="home">
+      <div
+        className="banner"
+        style={{
+          backgroundImage: `url(${myBanner})`,
+        }}
+      >
+        <h1>GOAT</h1>
+        <p>“GOAT” is an animated sports‑comedy about Will Harris, a small but determined goat who dreams of playing professional roarball — a fast‑paced, full‑contact sport in a world of anthropomorphic animals. Despite being undersized, Will gets a rare chance to join a pro team and must prove that heart and talent matter more than size. The film blends high‑energy action, comedy, and heart‑warming underdog themes.</p>
+
+        <div>
+          {/* <button onClick={() => console.log("CLICKED")}> */}
+          <button onClick={() => window.location.reload()}>
+            <BiPlay /> Play
+          </button>
+          <button onClick={() => window.location.reload()}>
+            My List <AiOutlinePlus />
+          </button>
+        </div>
+      </div>
+
+      <Row title={"Upcoming"} arr={upcomingMovies} />
+      <Row title={"Now Playing"} arr={nowPlayingMovies} />
+      <Row title={"Popular"} arr={popularMovies} />
+      <Row title={"Top Rated"} arr={topRatedMovies} />
+
+      <div className="genreBox">
+        {genre.map((item) => (
+          <Link key={item.id} to={`/genre/${item.id}`}>
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
